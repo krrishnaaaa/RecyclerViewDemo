21b791fda004a22949f0e6c07c19637da1bce41d
krrishnaaaa <krishna@pcsalt.com>
2015-06-24 01:40:44 +0530

    added click listener to RecyclerView

--- app/src/main/java/com/pcsalt/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/pcsalt/recyclerviewdemo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.pcsalt.recyclerviewdemo;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.pcsalt.recyclerviewdemo.listener.RecyclerClickListener;
+import com.pcsalt.recyclerviewdemo.listener.RecyclerTouchListener;
 
 import org.json.JSONObject;
 
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     AppCompatActivity activity = MainActivity.this;
+    List<PostValue> postList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +37,20 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(activity, recyclerView, new RecyclerClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                if (postList != null) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(postList.get(position).getGuid()));
+                    startActivity(intent);
+                }
+            }
+        }));
     }
 
     class JSONAsync extends AsyncTask<Void, Void, Void> {
         ProgressDialog pd;
-        List<PostValue> postList;
 
         @Override
         protected void onPreExecute() {
--- app/src/main/java/com/pcsalt/recyclerviewdemo/PostValue.java
+++ app/src/main/java/com/pcsalt/recyclerviewdemo/PostValue.java
@@ -1,29 +1,29 @@
 package com.pcsalt.recyclerviewdemo;
 
 public class PostValue {
-	String title, guid, date;
+    private String title, guid, date;
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getGuid() {
-		return guid;
-	}
+    public String getGuid() {
+        return guid;
+    }
 
-	public void setGuid(String guid) {
-		this.guid = guid;
-	}
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
